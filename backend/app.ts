4acd7b444942ff03ad6a674b2aa04dd42ffaca49
Hangeony <geonhui95@naver.com>
2025-06-12 15:57:23 +0900

    feat : 기본 미들웨어 작성

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,15 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
-console.log(greet("TypeScript"));
+const app = express();
+const port = 3000;
 
+//기본 미들웨어
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`${port} 번으로 실행중`);
+});
