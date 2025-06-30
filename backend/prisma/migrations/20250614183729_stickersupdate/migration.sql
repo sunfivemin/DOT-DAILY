3a42300662e7cae11054a3c80c23586d2ecac97f
Hangeony <geonhui95@naver.com>
2025-06-30 23:28:58 +0900

    배포설정하기

--- backend/prisma/migrations/20250614183729_stickersupdate/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `categoryId` on the `Stickers` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Stickers` DROP COLUMN `categoryId`;
