7b441f73f9e5ec685fdd02326f4712a89a490ba8
Hangeony <geonhui95@naver.com>
2025-07-08 15:54:40 +0900

    feat: retry 로직 구현

--- backend/app.ts
+++ backend/app.ts
@@ -5,6 +5,7 @@ import { errorNotFoundHandler, errorHandler } from './middlewares/errorHandler';
 import { swaggerSpec } from './docs';
 import swaggerUi from 'swagger-ui-express';
 import routers from './router/index';
+import { startBatchJobs } from './jobs/cron';
 
 const app = express();
 const port = 3000;
@@ -17,13 +18,18 @@ app.use(express.json());
 // 라우터
 app.use('/api/v1', routers);
 
+// 배포시 404 에러 방지
 app.get('/', (req, res) => {
   res.json({ message: 'ok' });
 });
 
+// 서버 죽는거 방지
 app.get('/healthz', (req, res) => {
   res.json({ message: 'ok' });
 });
+
+//서버 시작 시 배치 스케줄 등록
+startBatchJobs();
 
 // swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
---
+++ backend/jobs/cron.ts
@@ -0,0 +1,22 @@
+import cron from 'node-cron';
+import { processExpiredTodos } from '../service/todoBatch.service';
+
+/**
+ *  모든 배치 스케줄 등록
+ */
+export const startBatchJobs = () => {
+  // '0 0 * * *' 자정마다 실행
+  // 밑에 코드는 1분마다 실행 test
+  cron.schedule('*/1 * * * *', async () => {
+    console.log('[배치] 자정 배치 작업 실행 시작');
+    try {
+      const result = await processExpiredTodos();
+      console.log('[배치] 처리 결과:', result);
+    } catch (error) {
+      console.error('[배치] 처리 중 오류 발생:', error);
+    }
+  });
+
+  // 다른 배치 스케줄 추가 가능
+  // cron.schedule('0 3 * * *', someOtherBatchJob); // 매일 오전 3시에 실행
+};
