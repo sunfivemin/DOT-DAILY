e5fa2c5befa1dc8deec063de2d6ea009bedfe9d4
Hangeony <geonhui95@naver.com>
2025-07-10 13:39:59 +0900

    style: daily-review roter 우선순위 변경

--- backend/router/dailyReview.router.ts
+++ backend/router/dailyReview.router.ts
@@ -14,11 +14,11 @@ const router = express.Router();
 // 회고 저장
 router.post('/', authenticate, createDailyReviewController);
 
-// 회고 전체 조회
-router.get('/:date', authenticate, getDailyReviewController);
-
 // 회고 월별 조회
 router.get('/month', authenticate, getMonthlyReviewsController);
+
+// 회고 전체 조회
+router.get('/:date', authenticate, getDailyReviewController);
 
 // 회고 수정
 router.put('/:date', authenticate, updateDailyReviewController);
